0485e3ce3ac0d5b09af28d07914bd0a41c09a8b1
Hussein Adams <hussein.a.adams@gmail.com>
2020-11-30 17:13:00 -0800

    don't need to remove query embeddings if not changed

--- main.py
+++ main.py
@@ -178,7 +178,6 @@ def main(args):
 
         del checkpoint['model']['class_embed.weight']
         del checkpoint['model']['class_embed.bias']
-        del checkpoint['model']['query_embed.weight']
         model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
